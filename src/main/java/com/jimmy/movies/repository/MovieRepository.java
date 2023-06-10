9fe4d5569153d88c1c08da5f8f6f60a396aae355
jimmy-kyalo <kyalo90@gmail.com>
2023-06-10 14:49:24 +0300

    endpoints

---
+++ src/main/java/com/jimmy/movies/repository/MovieRepository.java
@@ -0,0 +1,13 @@
+package com.jimmy.movies.repository;
+
+import com.jimmy.movies.domain.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
