9a8a338252190014fc6ca33a9f4a1c1ac0a7b769
jimmy-kyalo <kyalo90@gmail.com>
2023-06-12 16:22:02 +0300

    review update

---
+++ src/main/java/com/jimmy/movies/repository/ReviewRepository.java
@@ -0,0 +1,8 @@
+package com.jimmy.movies.repository;
+
+import com.jimmy.movies.domain.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
